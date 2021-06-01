2f9e270edbed23dd18cd9f4864780a5a10a2f7e7
Midhun-19 <midhunkumar6780@gmail.com>
2021-06-01 14:17:52 +0530

    Two functions- wallis and monte_carlo - for estimating the value of pi have been written in python.

--- estimate.py
+++ estimate.py
@@ -1,12 +1,30 @@
 import math
 import unittest
+import random
+
+def wallis(n):
+    k=1;
+    for i in range(1,n):
+        k=k*(4*i**2)/(4*i**2-1)
+    return 2*k
+        
+def monte_carlo(n):
+    x=0
+    for i in range(n):
+        a=random.random()
+        b=random.random()
+        if (a**2 + b**2)<=1 :
+            x+=1
+
+    return 4*x/n
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
             self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
+
     def test_high_iters(self):
         for i in range(500, 600):
             pi = wallis(i)
@@ -17,7 +35,7 @@ class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
+
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
         self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
@@ -26,7 +44,6 @@ class TestMC(unittest.TestCase):
         for i in range(500, 600):
             pi = monte_carlo(i)
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+
 if __name__ == "__main__":
     unittest.main()
